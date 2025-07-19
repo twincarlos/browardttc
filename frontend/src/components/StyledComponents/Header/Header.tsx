aa940e3360b3b6d5225bf445ccfce1daa9d20232
Carlos Rodriguez <crodriguez@biocollections.com>
2025-07-19 03:06:10 -0400

    checkpoint

--- frontend/src/components/StyledComponents/Header/Header.tsx
+++ frontend/src/components/StyledComponents/Header/Header.tsx
@@ -1,9 +1,13 @@
 import './Header.css';
+import SettingsIcon from '../Icons/SettingsIcon';
 
 export default function Header({title}: {title: string}) {
     return (
         <header className="ta-c">
             <h1>{title}</h1>
+            <button className="header-button">
+                <SettingsIcon />
+            </button>
         </header>
     )
 }
